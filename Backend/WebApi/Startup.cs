c525a3c5107549b00451a7f3deaeca835d86bb9f
Nicholas Myers <neekgreen@users.noreply.github.com>
2017-11-06 20:22:55 -0600

    api updates

--- Backend/WebApi/Startup.cs
+++ Backend/WebApi/Startup.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace WebApi
+﻿namespace WebApi
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Authorization;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using AutoMapper;
+    using FluentValidation.AspNetCore;
+    using Newtonsoft.Json.Serialization;
+    using MediatR;
+    using Swashbuckle.AspNetCore.Swagger;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -20,13 +21,58 @@ namespace WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore(options =>
+            {
+                options.Filters.Add(
+                    new AuthorizeFilter(
+                        new AuthorizationPolicyBuilder()
+                            .RequireAuthenticatedUser()
+                            .Build()));
+            })
+            .UsePaginableHeaders()
+            .AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                    new CamelCasePropertyNamesContractResolver();
+            })
+            .AddApiExplorer()
+            .AddFluentValidation(u => u.RegisterValidatorsFromAssemblyContaining<Startup>())
+            .UsePaginableHeaders(u => u.UseExpanded());
+
+            services.AddAuthorization();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:50000";
+                    options.Audience = "api";
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.CustomSchemaIds(x => x.FullName);
+
+                var schema = new OAuth2Scheme
+                {
+                    Type = "apiKey",
+                    Description = "JWT token based authorization"
+                };
+                //schema.Extensions.Add("in", "header");
+                //schema.Extensions.Add("name", "Authorization");
+                //c.AddSecurityDefinition("api_key", schema);
+
+                //c.DocumentFilter<SwaggerAuthorizationFilter>();
+            });
+
+            services.AddAutoMapper();
+            Mapper.AssertConfigurationIsValid();
+
+            services.AddMediatR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,6 +81,12 @@ namespace WebApi
             }
 
             app.UseMvc();
+            app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
